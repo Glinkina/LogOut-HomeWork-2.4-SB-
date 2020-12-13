6ef0085a1767ac44b61862a2ade2ba707161f6f5
Andrey <glinkin.a.o@yandex.ru>
2020-12-14 00:58:54 +1000

    Home Work 2.4 Не готова.

--- LogOut HomeWork 2.4 (SB)/ViewController.swift
+++ LogOut HomeWork 2.4 (SB)/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var userNameField: UITextField!
+    @IBOutlet var userPasswordField: UITextField!
+    
+    private let userName = "Earth"
+    private let password = "1234"
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "Log In" else { return }
+    }
+    
+    @IBAction func forgotPass(_ sender: Any) {
+        popUpWindow(title: "Ready?? 🧑🏼‍🚀", message: "The name of the planet you live on!")
+    }
+    @IBAction func forgotName(_ sender: Any) {
+        popUpWindow(title: "Ready?? 🚀", message: "Сount from 4 to 1, vice versa!")
+    }
+    
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        guard
+            userNameField.text == userName && userPasswordField.text == password
+        else {
+            popUpWindow(title: "Unsuccessfully", message: "Username or password entered incorrectly")
+            return
+        }
+        performSegue(withIdentifier: "correctLogin", sender: nil)
+    }
+    
 
-
+}
+extension ViewController {
+    private func popUpWindow(title: String, message: String) {
+        let windom = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let pushAction = UIAlertAction(title: "OK", style: .default) {_ in
+        }
+        windom.addAction(pushAction)
+        present(windom, animated: true)
+    }
 }
 
---
+++ LogOut HomeWork 2.4 (SB)/WelcomeSecondPage.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeSecondPage.swift
+//  LogOut HomeWork 2.4 (SB)
+//
+//  Created by Andrey Glinkin on 13.12.2020.
+//
+
+import UIKit
+
+class WelcomeSecondPage: UIViewController {
+    var userName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
